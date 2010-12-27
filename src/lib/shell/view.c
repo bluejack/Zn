3a95be1b1c40a10aa00ba42de71a1c5614807a73
bluejack <bluejack@bluejack.com>
2010-12-26 18:47:19 -0800

    First draught of capturing arrow keys, but I don't like it. It needs deeper understanding of termcap/terminfo.

--- src/lib/shell/view.c
+++ src/lib/shell/view.c
@@ -5,6 +5,14 @@
 #include <termios.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+/* Local declarations */
+int _process_char(char, char*);
+
+void _add_char(char, unsigned int, char*);
+void _reset_esc_seq(bool*, unsigned short*, unsigned long*);
+
 
 static struct termios default_settings;
      
@@ -39,30 +47,13 @@ size_t fetch_line(char* buffer, size_t buffer_len) {
 
   unsigned int i = 0;
   unsigned int max = buffer_len - 1;
-  fd_set inputfds;
-  unsigned int stdinfd = fileno(stdin);
-  char linebuf[128];
-  char linemax = 128;
   char c = 0;
 
+  _process_char(0, NULL);
+
   do {
-    FD_ZERO(&inputfds);
-    FD_SET(stdinfd,&inputfds);
-
-    select(stdinfd+1, &inputfds, NULL, NULL, NULL);
-
-    ssize_t found = read(stdinfd, linebuf, linemax);
-
-    if (found > 0) {
-      c = linebuf[0];
-      if (found == 1) {
-	buffer[i++] = c;
-	putc(c, stdout);
-	fflush(stdout);
-      } else {
-	printf("Got a block.\n");
-      }
-    }
+    c = getchar();
+    i += _process_char(c, buffer);
   } while ( i < max && c != '\n' );
   
   buffer[i] = '\0';
@@ -86,6 +77,88 @@ void   display_err(const char* format, ...) {
   va_start(ap, format);
 
   vfprintf(stderr, format, ap);
+  
   va_end(ap);
 }
 
+int
+_process_char(char c, char* buffer) {
+
+  static unsigned int i = 0;
+  static bool esc_seq = false;
+  static unsigned long keycode = 0;
+  static unsigned short pos = 0;
+  unsigned int oldpos = i;
+
+  if (c == 0) {
+    i = 0;
+    esc_seq = false;
+    keycode = 0;
+    pos = 0;
+  } else {
+
+    /* This is an unsustainable approach. */
+    switch (c) {
+    case 27: /* ESC */
+      esc_seq = true;
+      pos = 1;
+      break;
+    case '[':
+      if (esc_seq && pos == 1) {
+	keycode = '[' << 7;
+	pos++;
+      } else {
+	_reset_esc_seq(&esc_seq, &pos, &keycode);
+	_add_char(c, i++, buffer);
+      }
+      break;
+    case 'A':
+      if (esc_seq && pos == 2) {
+	printf("[UP ARROW]");
+	fflush(stdin);
+      } else {
+	_add_char(c, i++, buffer);
+      }
+      _reset_esc_seq(&esc_seq, &pos, &keycode);
+      break;
+    case 'D':
+      if (esc_seq && pos == 2) {
+	printf("[LEFT ARROW]");
+	fflush(stdin);
+      } else {
+	_add_char(c, i++, buffer);
+      }
+      _reset_esc_seq(&esc_seq, &pos, &keycode);
+      break;
+    case 'C':
+      if (esc_seq && pos == 2) {
+	printf("[RIGHT ARROW]");
+	fflush(stdin);
+      } else {
+	_add_char(c, i++, buffer);
+      }
+      _reset_esc_seq(&esc_seq, &pos, &keycode);
+      break;
+    default:
+      _reset_esc_seq(&esc_seq, &pos, &keycode);
+      _add_char(c, i++, buffer);
+    }
+    
+  }
+
+  return i - oldpos;
+}
+
+void
+_add_char(char c, unsigned int pos, char* buffer) {
+  buffer[pos] = c;
+  putc(c, stdout);
+  fflush(stdout);
+}
+
+void 
+_reset_esc_seq(bool *esc_seq, unsigned short *pos, unsigned long *keycode) {
+  *esc_seq = false;
+  *pos = 0;
+  *keycode = 0;
+}
